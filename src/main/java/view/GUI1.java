c9f108a30ea34b5afc53e945ebe48d45d8d098df
João Vitor Oliveira da Silva <100168573+jvitsilva@users.noreply.github.com>
2024-03-03 15:46:59 -0300

    Add new functions and a better layout

--- src/main/java/view/GUI1.java
+++ src/main/java/view/GUI1.java
@@ -8,16 +8,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.text.ParseException;
 
 import javax.swing.DropMode;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import java.awt.Cursor;
 
 public class GUI1 {
 	
@@ -44,73 +47,91 @@ public class GUI1 {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	
 	public GUI1() {
-		initialize();
+		try {
+			initialize();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws ParseException 
 	 */
-	private void initialize() {
+	private void initialize() throws ParseException {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(192, 192, 192));
-		frame.setBounds(100, 100, 587, 424);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setBackground(new Color(208, 232, 230));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setSize(587, 424);
+		frame.setLocationRelativeTo(null);  
 		
-		
-		
-		JLabel lblNewLabel1 = new JLabel("Bem vindo ao JavaMail");
-		lblNewLabel1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel1.setFont(new Font("Rockwell Extra Bold", Font.PLAIN, 20));
-		lblNewLabel1.setBounds(140, 35, 289, 43);
-		frame.getContentPane().add(lblNewLabel1);
-		
-		JLabel lblNewLabel2 = new JLabel("Primeiro, digite seu email e senha");
-		lblNewLabel2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel2.setBounds(191, 94, 182, 18);
-		frame.getContentPane().add(lblNewLabel2);
-		
-		JLabel lblNewLabel3 = new JLabel("(Não se preocupe, eles estarão seguros, ");
-		lblNewLabel3.setHorizontalTextPosition(SwingConstants.LEADING);
-		lblNewLabel3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel3.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblNewLabel3.setBounds(132, 121, 296, 19);
-		frame.getContentPane().add(lblNewLabel3);
 
-		JLabel lblMasTenhaCerteza = new JLabel("mas tenha certeza que estão corretos! )");
-		lblMasTenhaCerteza.setBounds(183, 144, 207, 14);
-		frame.getContentPane().add(lblMasTenhaCerteza);
+		JPanel panel = new JPanel();
+		panel.setBorder(UIManager.getBorder("OptionPane.border"));
+		panel.setBackground(new Color(0, 128, 128));
+		panel.setBounds(90, 25, 416, 367);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel4 = new JLabel("Email:");
-		lblNewLabel4.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel4.setBounds(159, 189, 58, 18);
-		frame.getContentPane().add(lblNewLabel4);
+		
+		
+		JLabel welcomeLabel = new JLabel("Bem vindo ao JavaMail");
+		welcomeLabel.setBounds(0, 11, 416, 48);
+		panel.add(welcomeLabel);
+		welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		welcomeLabel.setFont(new Font("Baskerville Old Face", Font.BOLD, 30));
+		
+		
+		JLabel label3 = new JLabel("mas tenha certeza que estão corretos.");
+		label3.setHorizontalAlignment(SwingConstants.CENTER);
+		label3.setHorizontalTextPosition(SwingConstants.CENTER);
+		label3.setBounds(85, 141, 249, 14);
+		panel.add(label3);
+				
+				
+				
+		
+				
+
+		JLabel emailLabel = new JLabel("Email:");
+		emailLabel.setBounds(74, 166, 58, 18);
+		panel.add(emailLabel);
+		emailLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
 		
 		textFieldEmail = new JTextField();
+		textFieldEmail.setBounds(74, 200, 260, 24);
+		panel.add(textFieldEmail);
+		textFieldEmail.setBorder(null);
+		textFieldEmail.setFont(new Font("Calibri", Font.PLAIN, 15));
 		textFieldEmail.setToolTipText("");
 		textFieldEmail.setHorizontalAlignment(SwingConstants.LEFT);
 		textFieldEmail.setDropMode(DropMode.INSERT);
-		textFieldEmail.setBounds(169, 222, 227, 24);
-		frame.getContentPane().add(textFieldEmail);
 		textFieldEmail.setColumns(10);
 		
 		
 		
-		JLabel lblNewLabel5 = new JLabel("Senha:");
-		lblNewLabel5.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel5.setBounds(158, 257, 58, 18);
-		frame.getContentPane().add(lblNewLabel5);
-		
+		JLabel senhaLabel = new JLabel("Senha:");
+		senhaLabel.setBounds(74, 235, 58, 18);
+		panel.add(senhaLabel);
+		senhaLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+				
 		passwordField = new JPasswordField();
+		passwordField.setBounds(75, 264, 259, 24);
+		panel.add(passwordField);
+		passwordField.setBorder(null);
+		passwordField.setFont(new Font("Calibri", Font.PLAIN, 15));
 		passwordField.setHorizontalAlignment(SwingConstants.LEFT);
-		passwordField.setBounds(170, 286, 227, 24);
-		frame.getContentPane().add(passwordField);
 		
+
+		// Exibicão da senha
 		JCheckBox exibirSenha = new JCheckBox("Mostrar");
+		exibirSenha.setOpaque(false);
 		exibirSenha.setBackground(new Color(192, 192, 192));
 		exibirSenha.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
@@ -124,15 +145,42 @@ public class GUI1 {
 		}
 		}
 		});
-		exibirSenha.setBounds(414, 287, 97, 23);
+		exibirSenha.setBounds(374, 323, 71, 23);
 		frame.getContentPane().add(exibirSenha);
-			
+				
 		
 		
+		// Botões
+		buttonNext = new JButton("Log In");
+		buttonNext.setBounds(153, 299, 101, 27);
+		panel.add(buttonNext);
+		buttonNext.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		buttonNext.setBorder(null);
+		buttonNext.setBackground(new Color(208, 232, 230));
 		
-		buttonNext = new JButton("Avançar");
-		buttonNext.setBounds(224, 331, 101, 27);
-		frame.getContentPane().add(buttonNext);
+		JButton sairButton = new JButton("Sair");
+		sairButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		sairButton.setBorder(null);
+		sairButton.setBackground(new Color(208, 232, 230));
+		sairButton.setBounds(153, 337, 101, 23);
+		panel.add(sairButton);
+		
+		JLabel label1 = new JLabel("Login de acesso");
+		label1.setBounds(109, 66, 207, 24);
+		panel.add(label1);
+		label1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label1.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel label2 = new JLabel("Não se preocupe, eles estarão seguros, ");
+		label2.setBounds(65, 118, 296, 19);
+		panel.add(label2);
+		label2.setHorizontalTextPosition(SwingConstants.CENTER);
+		label2.setHorizontalAlignment(SwingConstants.CENTER);
+		label2.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		
 		
 		
@@ -145,12 +193,11 @@ public class GUI1 {
 				password = new String(passwordField.getPassword());
 				window2.recebeDados(email, password);
 				
+				frame.dispose(); //desabilita o primeiro frame
+				
 				EventQueue.invokeLater(new Runnable() {
 							public void run() {
 								try {
-									System.out.println("aqui1");
-									System.out.println(email+password);
-									System.out.println("aqui2");
 									
 									window2.frame.setVisible(true);
 									
@@ -168,5 +215,4 @@ public class GUI1 {
 		
 		
 	}
-	
 }
--- src/main/java/view/GUI2.java
+++ src/main/java/view/GUI2.java
@@ -10,8 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.border.BevelBorder;
 import javamail.envio.ObjetoEnviaEmail;
+import javax.swing.JCheckBox;
 
 public class GUI2 {
 
@@ -46,13 +46,15 @@ public class GUI2 {
 	 */
 	private void initialize() {
 		
-		System.out.println(user + password);
-		System.out.println("aqui3");
+		
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(192, 192, 192));
-		frame.setBounds(100, 100, 587, 424);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setBackground(new Color(64, 128, 128));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setSize(587, 424);
+		frame.setLocationRelativeTo(null);  
 		
 		
 		JLabel destinoLabel = new JLabel("Destinatários:");
@@ -61,6 +63,7 @@ public class GUI2 {
 		frame.getContentPane().add(destinoLabel);
 
 		destinatarios = new JTextField();
+		destinatarios.setBorder(null);
 		destinatarios.setBackground(new Color(255, 255, 255));
 		destinatarios.setBounds(149, 48, 302, 29);
 		frame.getContentPane().add(destinatarios);
@@ -74,6 +77,7 @@ public class GUI2 {
 		frame.getContentPane().add(remetenteLabel);
 
 		remetente = new JTextField();
+		remetente.setBorder(null);
 		remetente.setBounds(149, 110, 302, 29);
 		frame.getContentPane().add(remetente);
 		remetente.setColumns(10);
@@ -86,6 +90,7 @@ public class GUI2 {
 		frame.getContentPane().add(assuntoLabel);
 
 		assunto = new JTextField();
+		assunto.setBorder(null);
 		assunto.setBounds(149, 175, 302, 29);
 		frame.getContentPane().add(assunto);
 		assunto.setColumns(10);
@@ -93,6 +98,9 @@ public class GUI2 {
 		
 		
 		conteudo = new JTextField();
+		conteudo.setBorder(null);
+		conteudo.setEnabled(true);
+		conteudo.setDisabledTextColor(new Color(177, 177, 177));
 		conteudo.setHorizontalAlignment(SwingConstants.LEFT);
 		conteudo.setBounds(149, 240, 302, 76);
 		frame.getContentPane().add(conteudo);
@@ -100,20 +108,62 @@ public class GUI2 {
 		
 		
 		
-		JLabel lblNewLabel_3 = new JLabel("(nome)");
-		lblNewLabel_3.setBounds(461, 117, 46, 14);
-		frame.getContentPane().add(lblNewLabel_3);
+		JLabel nomeLabel = new JLabel("(nome)");
+		nomeLabel.setBounds(461, 117, 46, 14);
+		frame.getContentPane().add(nomeLabel);
 		
-		JLabel lblNewLabel_4 = new JLabel("(e-mails)");
-		lblNewLabel_4.setBounds(461, 55, 69, 14);
-		frame.getContentPane().add(lblNewLabel_4);
+		JLabel emailsLabel = new JLabel("(e-mails)");
+		emailsLabel.setBounds(461, 55, 69, 14);
+		frame.getContentPane().add(emailsLabel);
+		
+		
+		
+		JCheckBox msgAuto = new JCheckBox("Mensagem automática");
+		msgAuto.setFont(new Font("Tahoma", Font.BOLD, 10));
+		
+		msgAuto.setHorizontalAlignment(SwingConstants.LEFT);
+		msgAuto.setOpaque(false);
+		msgAuto.setVerticalTextPosition(SwingConstants.BOTTOM);
+		msgAuto.setBounds(383, 331, 161, 41);
+		frame.getContentPane().add(msgAuto);
+		
+		msgAuto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(msgAuto.isSelected()) {
+					conteudo.setBackground(new Color(192, 192, 192));
+					conteudo.setEnabled(false);
+					conteudoString = strBuilder();
+				} else {
+					conteudo.setBackground(new Color(255, 255, 255));
+					conteudo.setEnabled(true);
+					conteudoString = conteudo.getText();
+					}
+			}
+		});
+		
+		
+		
+		// Botões
+		
+		JButton sairButton = new JButton("Sair");
+		sairButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		sairButton.setBackground(new Color(208, 232, 230));
+		sairButton.setBorder(null);
+		sairButton.setBounds(253, 374, 89, 23);
+		frame.getContentPane().add(sairButton);
 		
 		
 		
 		JButton enviarButton = new JButton("Enviar");
-		enviarButton.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		enviarButton.setBackground(new Color(208, 232, 230));
+		enviarButton.setBorder(null);
 		enviarButton.setBounds(253, 340, 89, 23);
 		frame.getContentPane().add(enviarButton);
+		
 		
 		enviarButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -121,7 +171,7 @@ public class GUI2 {
 				destinatarioString = destinatarios.getText();
 				remetenteString = remetente.getText();
 				assuntoString = assunto.getText();
-				conteudoString = conteudo.getText();
+				
 				
 				ObjetoEnviaEmail enviaEmail = 
 	                    new ObjetoEnviaEmail(user,
@@ -132,7 +182,13 @@ public class GUI2 {
 	                                        conteudoString);
 
 	            try {
-					enviaEmail.enviarEmail(false);
+	            	if(msgAuto.isSelected()) {
+	            		enviaEmail.enviarEmail(true);
+	            		System.exit(0);
+	            	} else {
+	            		enviaEmail.enviarEmail(false); 
+	            		System.exit(0);
+	            	}
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -157,10 +213,9 @@ public class GUI2 {
         stringBuilderTextoEmail.append("Se você está lendo isto, quer dizer que o projeto está funcionando normalmente <br><br/>");
         stringBuilderTextoEmail.append("Aperte no botão abaixo, você será redirecionado para um site com uma imagem aleatória <br><br/>");
 
-        stringBuilderTextoEmail.append("<a target=\"_blank\" href=\"https://random.imagecdn.app/5000/2000\" style=\"color:#000000; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:30px; font-size: 20px; font-family: courier; border: 3px blue; background-color:#CCCCCC;\" >Clique aqui</a><br><br/>");
+        stringBuilderTextoEmail.append("<a target=\"_blank\" href=\"https://www.youtube.com/watch?v=c4rfVjwsez4\" style=\"color:#000000; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:30px; font-size: 20px; font-family: courier; border: 3px blue; background-color:#CCCCCC;\" >Clique aqui</a><br><br/>");
         
         stringBuilderTextoEmail.append("<span style=\"font-size:5px\">Projeto feito por João Vitor Silva.</span>");
         return stringBuilderTextoEmail.toString();
 	}
-
 }
